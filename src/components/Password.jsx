67a3ea7cba3f8c369e862fb16e60d30e8d589e38
famich89 <famich89@gmail.com>
2024-11-11 14:47:26 -0500

    Safe Changes

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,44 +1,46 @@
-import { useRef } from "react";
+import { useRef } from 'react';
 
-useRef
-export default function Password({ password, setPassword, correctPassword, setIsLoggedIn, error, setError }) {
-    const ref = useRef();
+useRef;
+export default function Password({
+  password,
+  setPassword,
+  correctPassword,
+  setIsLoggedIn,
+  error,
+  setError,
+}) {
+  const ref = useRef();
 
-    const handleLogin = () => {
-        if (password === correctPassword) {
-          setIsLoggedIn(true);
-          setError(''); // Clear any previous errors
-        } else {
-          setError('Incorrect password. Please try again.');
-        }
-    };
+  const handleLogin = () => {
+    if (password === correctPassword) {
+      setIsLoggedIn(true);
+      setError(''); // Clear any previous errors
+    } else {
+      setError('Incorrect password. Please try again.');
+    }
+  };
 
-    const addPassword = () => {
-        setPassword(ref.current.value);
-     }
-    
-    return ( 
-      <section className="welcome">
-<<<<<<< HEAD
-            <h2>Please Enter Password</h2>
-=======
+  const addPassword = () => {
+    setPassword(ref.current.value);
+  };
+
+  return (
+
+    <section className="welcome">
             <h2>Please enter password</h2>
->>>>>>> cb22098 (Safe Version)
-            <div className="enter">
-                    <input
-                    type="password"
-                    ref={ref}
-                    onChange={(e) => addPassword()}
-                    placeholder="Enter password" />
-            </div>
-            <div>
-<<<<<<< HEAD
-                <button onClick={handleLogin}>Log In</button>
-=======
-                <button onClick={handleLogin}>LOG IN</button>
->>>>>>> cb22098 (Safe Version)
-                </div>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-                </section>
-    );
+
+      <div className="enter">
+        <input
+          type="password"
+          ref={ref}
+          onChange={(e) => addPassword()}
+          placeholder="Enter password"
+        />
+      </div>
+      <div>
+        <button onClick={handleLogin}>LOG IN</button>
+      </div>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </section>
+  );
 }
