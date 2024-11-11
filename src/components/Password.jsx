2c115be01760b3dfba820810f8248d6c2d0d3710
famich89 <famich89@gmail.com>
2024-11-11 13:33:15 -0500

    Safe Version

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -19,7 +19,11 @@ export default function Password({ password, setPassword, correctPassword, setIs
     
     return ( 
       <section className="welcome">
+<<<<<<< HEAD
             <h2>Please Enter Password</h2>
+=======
+            <h2>Please enter password</h2>
+>>>>>>> cb22098 (Safe Version)
             <div className="enter">
                     <input
                     type="password"
@@ -28,7 +32,11 @@ export default function Password({ password, setPassword, correctPassword, setIs
                     placeholder="Enter password" />
             </div>
             <div>
+<<<<<<< HEAD
                 <button onClick={handleLogin}>Log In</button>
+=======
+                <button onClick={handleLogin}>LOG IN</button>
+>>>>>>> cb22098 (Safe Version)
                 </div>
         {error && <p style={{ color: 'red' }}>{error}</p>}
                 </section>
